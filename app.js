141d91229055a644795980bbc769741658899f84
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-08-05 17:50:55 +0430

    fix

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
-
-// set port to 3000 if you want to connect locally
-const port = "https://task-manager-tau-snowy.vercel.app/"
+// if you want run locally uncommented port and app.listen
+// const port = 3000
 
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is listening on ${port}`);
+// });
 
 // Connect to the database
 connectDB(process.env.MONGO_DB)
